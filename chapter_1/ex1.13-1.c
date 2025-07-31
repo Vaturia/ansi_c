b97ebda80abba0a769646456999147cc3cd2277f
Vaturia <kkorzun19@gmail.com>
2025-07-31 18:20:17 +0300

    Added solution for ex 1.13-1

---
+++ chapter_1/ex1.13-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define MAX_COUNT_SYMBOLS 20
+
+int main()
+{
+    int words[MAX_COUNT_SYMBOLS + 1] = {0}, c, length_word = 0;
+
+    while((c = getchar()) != EOF)
+    {
+        if(c != '\n' && c != '\t' && c != ' ' && c != ',' && c != '.')
+        {
+            ++length_word;
+
+        }
+        else
+        {
+            if(length_word > 20)
+            {
+                ++words[20];
+            }
+            else
+            {
+                ++words[length_word - 1];
+            }
+            length_word = 0;
+        }
+    }
+
+    for(int i = 0; i < MAX_COUNT_SYMBOLS + 1; ++i)
+    {
+        if(i == 20)
+        {
+            printf("Words with >20 symbols: ");
+        }
+        else
+        {
+            printf("Words with %d symbols:", i + 1);
+        }
+
+        for(int j = 0; j < words[i]; ++j)
+        {
+            printf("%c", '*');
+        }
+        printf("%c", '\n');
+    }
+}
